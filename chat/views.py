ecae73f63bb709a3314bd01f27381a3b21864b49
MichaelSoquat <michael.soquat@gmx.de>
2022-04-23 12:19:21 +0200

    chat project

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        print("Received data " + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text= request.POST['textmessage'], chat=myChat, author=request.user, receiver = request.user)
+    return render(request, 'chat/index.html', {'username': 'Michael'} )
